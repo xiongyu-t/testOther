d0154915ad5199b05966182cbcc5b14244105083
覃雄钰 <1935266131@qq.com>
2023-05-15 14:25:56 +0800

    Initial commit

---
+++ src/main/java/com/qin/test/test1/MyGenericInterface.java
@@ -0,0 +1,6 @@
+package com.qin.test.test1;
+
+public interface MyGenericInterface<T> {
+
+    public T myMethod(T t);
+}
---
+++ src/main/java/com/qin/test/test1/Test.java
@@ -0,0 +1,40 @@
+package com.qin.test.test1;
+
+public class Test {
+
+    public static void main(String[] args) {
+        Test test = new Test();
+//        String[] strArr = {"hello", "world"};
+//        mgm.printArray(strArr); // 输出hello 和 world
+
+//        int i = aTest();
+//        System.out.println(i);
+
+        //获取一个类的class对象
+        Class<?> aClass = test.getClass();
+
+
+    }
+
+    // 泛型方法，T代表任意类型
+    public <T> void printArray(T[] arr){
+        for (T element : arr){
+            System.out.println(element);
+        }
+    }
+
+    public static int aTest(){
+        int a = 1;
+        try {
+            a = 2;
+            return a;
+
+        }catch (Exception e){
+            a = 3;
+            return a;
+        }finally {
+            a = 4;
+            return a;
+        }
+    }
+}
